e24c426545eef681cf53cac1072a7c27c8ea2e4e
c-pill <81812824+c-pill@users.noreply.github.com>
2024-06-23 18:25:11 -0400

    snake

--- README.txt
+++ README.txt
@@ -1,4 +1,10 @@
-Simple game of snake based off tutorials: https://youtu.be/E_-lMZDi7Uw?si=Jn15JFn5SCx8O10D
+Game of snake in terminal.
 
-to compile: g++ snake.cpp -o snake
-to run: ./snake
+Tutorials used for implementation of base game: https://youtu.be/E_-lMZDi7Uw?si=Jn15JFn5SCx8O10D
+
+Added features for better look/experience
+
+In terminal:
+
+    to compile: g++ snake.cpp -o snake
+    to run: ./snake
--- snake.cpp
+++ snake.cpp
@@ -1,10 +1,11 @@
 #include <conio.h>
 #include <iostream>
 #include <unistd.h>
+#include <windows.h>
 
 using namespace std;
 
-const int width = 20;
+const int width = 60;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100];
@@ -14,13 +15,15 @@ enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
 bool gameOver;
+bool quit; 
 
 void Setup()
 {
     gameOver = false;
+    quit = false;
     dir = STOP;
-    x = width / 2;
-    y = height /2;
+    x = width - (width / 2) -1 ;
+    y = height - (height / 2);
     srand(time(NULL));
     fruitX = (rand() % (width-1)) + 1;
     fruitY = (rand() % (height-1)) + 1;
@@ -28,26 +31,59 @@ void Setup()
     nTail = 0;
 }
 
-void Draw()
+void Border() 
 {
     system("cls");
-    for (int i = 0; i <= width; i++) 
+    for (int i = 0; i <= width; i++)
         cout << '#';
     cout << endl;
-
     for (int i = 0; i < height; i++) {
-        for (int j = 0; j <= width; j++) {
-            if (j == 0 || j == width)
-                cout << '#';
-            else if (i == y && j == x)
-                cout << '0';
-            else if (i == fruitY && j == fruitX)
-                cout << 'F';
+        cout << '#';
+        for (int j = 0; j < width-1; j++) {
+            if (i == y-1 && j == x)
+                cout << "\e[1;32m0\e[1;33m";
+            else
+                cout << ' ';
+        }
+        cout << '#';
+        if (i < 4)
+            cout << "\e[1;30m";
+        if (i == 0)
+            cout << "\t\t\tCommands:";
+        else if (i == 1)
+            cout << "\t\tWASD or Arrow Keys to move snake";
+        else if (i == 2)
+            cout << "\t\tPress x to quit";
+        else if (i == 3)
+            cout << "\t\tPress FN11 to toggle fullscreen";
+        if (i < 4)
+            cout << "\e[1;33m";
+        cout << endl;
+    }
+    for (int i = 0; i <= width; i++)
+        cout << '#';
+    cout << endl;
+    for (int i = 0; i <= height; i++)
+        cout << "\x1b[A";
+    cout << '#';
+    cout << "\x1b[s";
+}
+
+void Draw()
+{
+    cout << "\x1b[u";
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j < width; j++) {
+            if (i == y && j == x) 
+                cout << "\e[1;32m0\e[1;33m";
+            else if (i == fruitY && j == fruitX) {
+                cout << "\e[1;31mF\e[1;33m";
+            }
             else {
                 bool print = false;
                 for (int k = 0; k < nTail && !print; k++) {
                     if (tailX[k] == j && tailY[k] == i) {
-                        cout << 'o';
+                        cout << "\e[1;32mo\e[1;33m";
                         print = true;
                     }
                 }
@@ -55,11 +91,9 @@ void Draw()
                     cout << ' ';
             }
         }
-        cout << endl;
+        cout << "\x1b[B";
+        cout << "\r#";
     }
-
-    for (int i = 0; i <= width; i++)
-        cout << '#';
     
     cout << endl << "Score: " << score << endl;
 }
@@ -70,19 +104,24 @@ void Input()
     {
         switch (_getch()) {
             case 'w': 
+            case 72:
                 dir = UP;
                 break;
             case 'a':
+            case 75:
                 dir = LEFT;
                 break;
             case 's':
+            case 80:
                 dir = DOWN;
                 break;
             case 'd':
+            case 77:
                 dir = RIGHT;
                 break;
             case 'x':
                 gameOver = true;
+                quit = true;
                 break;
             default:
                 break;
@@ -110,36 +149,36 @@ void Logic()
     switch(dir) {
         case LEFT:
             x--;
-            usleep(100000);
+            usleep(15000);
             Draw();
             break;
         case RIGHT: 
             x++;
-            usleep(100000);
+            usleep(15000);
             Draw();
             break;
         case UP:
             y--;
-            usleep(100000);
+            usleep(30000);
             Draw();
             break;
         case DOWN:
             y++;
-            usleep(100000);
+            usleep(30000);
             Draw();
             break;
         default:
             break;
     }
-    if (x == 0 || x == width || y == 0 || y == height) {
+    if (x == 0 || x == width || y == 0 || y == height+1) {
         gameOver = true;
-        cout << "Out of bounds";
+        cout << endl << "Out of bounds";
     }
 
     for (int i = 0; i < nTail && !gameOver; i++) {
         if (tailX[i] == x && tailY[i] == y) {
             gameOver = true;
-            cout << "Watch your tail!";
+            cout << endl << "Watch your tail!";
         }
     }
     if (x == fruitX && y == fruitY)
@@ -150,25 +189,59 @@ void Logic()
         fruitY = (rand() % (height-1)) + 1;
         nTail++;
     }
+    if (nTail == 100) 
+    {
+        gameOver = true;
+        cout << endl << "You're full! You win!";
+    }
+
 }
 
 int main()
 {
     bool play = true;
-    char option;
-    while (play) {
+    system("COLOR 0e");
+
+    while (play) 
+    {
+        cout << "\e[?25l";
         Setup();
-        Draw();
+        Border();
+        
         while (!gameOver)
         {
             Input();
             Logic();
         }
-        cout << endl << "Play again? (y/n): ";
-        cin >> option;
-        play = option == 'y' ? true : false;
+
+        if (!quit) {
+            cout << endl << endl << "Play again? (y/n): " << endl;
+        
+            bool cont = false;
+        
+            while (!cont)
+            {
+                switch (_getch()) {
+                    case 'y': 
+                        play = true;
+                        cont = true;
+                        break;
+                    case 'n':
+                        play = false;
+                        cont = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        play = false;
     }
+
+    cout << "\e[?25h";
     system("cls");
+    cout << "Rip snake" << endl;
+    system("COLOR 07");
 
     return 0;
 }
